e08b63deca79f23ab09de063ba96f8317593f40e
anandbiniya <anand.biniya@gmail.com>
2024-03-27 12:09:20 +0530

    added

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./Home.module.css"
+import Cater from "./Images/ManaVantalu.jpg"
+function Home() {
+  return (
+    <div className={styles.navbar} >
+     <div>
+        <img src={Cater}/>
+     </div>
+     <div className={styles.about}>
+     <nav>About Us</nav>
+        <nav>Services</nav>
+        <nav>Gallery</nav>
+     </div>
+       
+    </div>
+  )
+}
+
+export default Home
