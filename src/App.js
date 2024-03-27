7b5063c08086004ebcbb177ab8903795e76eead5
anandbiniya <anand.biniya@gmail.com>
2024-03-27 15:45:14 +0530

    what we do section added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Main from './components/Main';
+// import Ex from './components/ex';
 
 function App() {
   return (
     <div className="App">
      <Home/>
-     <Main/>
+   <Main/>
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import Hero from "./Images/Hero.jpg"
 import styles from "./Main.module.css"
+import service from "./Images/service.jpg"
 
 function Main() {
   return (
+    <div>
     <div className={styles.main}>
         <img src={Hero}/>
+    </div>
+    <h2>What We Do</h2>
+    <div className={styles.what_we_do}>
+      
+        <div>
+            <h4>Live Counters</h4>
+            <img src={service}/>
+        </div>
+        <div>
+            <h4>Live Counters</h4>
+            <img src={service}/>
+        </div>
+        <div>
+            <h4>Live Counters</h4>
+            <img src={service}/>
+        </div>
+       
+    </div>
+
     </div>
   )
 }
